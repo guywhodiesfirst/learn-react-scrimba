9424544473adfc962c3bc580b6cd69a4d488629a
guywhodiesfirst <brizeout@gmail.com>
2024-08-15 20:24:46 +0300

    add props to card component

--- section-2/src/App.jsx
+++ section-2/src/App.jsx
@@ -3,13 +3,28 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
 import "./App.css"
-
+import data from "../data"
 export default function App() {
-            // <Hero />
-    return (
-        <div>
-            <Navbar />
-            <Card />
-        </div>
-    )
+  // <Hero />
+const cardArray = data.map(cardInfo => (
+  <Card 
+    key={cardInfo.id}
+    img={cardInfo.coverImg}
+    rating={cardInfo.stats.rating}
+    reviewCount={cardInfo.stats.reviewCount}
+    location={cardInfo.location}
+    title={cardInfo.title}
+    price={cardInfo.price}
+    openSlots={cardInfo.openSpots}
+    description={cardInfo.description}
+  />
+))
+return (
+<div>
+  <Navbar />
+  <div>
+      {cardArray}
+  </div>
+</div>
+)
 }
