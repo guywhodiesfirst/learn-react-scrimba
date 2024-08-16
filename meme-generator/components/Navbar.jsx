cfeed4742ed3602c17c95e5c1a9e3a06bd50eca9
guywhodiesfirst <brizeout@gmail.com>
2024-08-16 20:41:41 +0300

    meme-generator: add navbar component

---
+++ meme-generator/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return(
+        <>
+            <nav className="purple-gradient">
+                <img src="/images/doge.png" alt="Logo"/>
+                <h2>Meme Generator</h2>
+            </nav>
+        </>
+    )
+}
